8ab3e392996762657354fc1f1539faf0ed2b2be6
ShouyangDong <shouyang.dong@gmail.com>
2025-09-17 14:35:24 +0800

    remove useless redundent kernels

--- KernelBench/C/gemv_125_312.cpp
+++
@@ -1,8 +0,0 @@
-extern "C" void gemv(float *A, float *x, float *y) {
-  for (int i = 0; i < 125; i++) {
-    y[i] = 0;
-    for (int j = 0; j < 312; j++) {
-      y[i] += A[i * 312 + j] * x[j];
-    }
-  }
-}
