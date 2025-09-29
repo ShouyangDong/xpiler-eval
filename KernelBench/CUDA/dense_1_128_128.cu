ff30f049cb5c5b12b0634ad652768e44861ea225
ShouyangDong <shouyang.dong@gmail.com>
2025-09-29 17:04:22 +0800

    fix the cuda dense kernel

--- KernelBench/CUDA/dense_1_128_128.cu
+++
@@ -1,8 +0,0 @@
-// Auto-generated: dense with args [1, 128, 128]
-// TODO: Implement kernel for dense_1_128_128.cu
-
-#include <iostream>
-
-void dense_1_128_128.cu() {
-  std::cout << "Running dense_1_128_128.cu..." << std::endl;
-}
