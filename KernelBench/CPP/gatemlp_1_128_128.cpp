d2e7e65f20a085d10977c34fc76231304a6df834
ShouyangDong <shouyang.dong@gmail.com>
2025-09-30 17:26:44 +0800

    add cpp gatemlp

--- KernelBench/CPP/gatemlp_1_128_128.cpp
+++
@@ -1,8 +0,0 @@
-// Auto-generated: gatemlp with args [1, 128, 128]
-// TODO: Implement kernel for gatemlp_1_128_128.cpp
-
-#include <iostream>
-
-void gatemlp_1_128_128() {
-  std::cout << "Running gatemlp_1_128_128.cpp..." << std::endl;
-}
--- KernelBench/CPP/gatemlp_1_512_512.cpp
+++
@@ -1,8 +0,0 @@
-// Auto-generated: gatemlp with args [1, 512, 512]
-// TODO: Implement kernel for gatemlp_1_512_512.cpp
-
-#include <iostream>
-
-void gatemlp_1_512_512() {
-  std::cout << "Running gatemlp_1_512_512.cpp..." << std::endl;
-}
