d2e1161d5250c6753b5e09711aa04e53e1a062f7
ShouyangDong <shouyang.dong@gmail.com>
2025-10-11 13:02:29 +0800

    remove useless comments

--- KernelBench/CPP/dense_16_1024_1024.cpp
+++ KernelBench/CPP/dense_16_1024_1024.cpp
@@ -10,12 +10,12 @@ extern "C" void dense(int16_t *A, int16_t *B, int32_t *bias, int32_t *C) {
       for (int k = 0; k < K; k += 32) {
         __m512i a_vec = _mm512_loadu_si512((__m512i *)&A[i * K + k]);
         for (int t = 0; t < 32; ++t)
-          b_tmp[t] = B[(k + t) * N + j]; // column-major
+          b_tmp[t] = B[(k + t) * N + j]; 
         __m512i b_vec = _mm512_load_si512((__m512i *)b_tmp);
         acc = _mm512_dpwssds_epi32(acc, a_vec, b_vec);
       }
 
-      // 水平加法 reduce 32个int32 -> 1个int32
+
       int32_t tmp[16];
       _mm512_store_si512((__m512i *)tmp, acc);
       int32_t sum = 0;
